6857c643f94ded984c3277c8a38fe4388512a058
enisgultekin <enis.gultekin@gmail.com>
2020-03-23 11:41:23 +0300

    Added MediatR Functionality

--- CleanArch/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArch.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CourseController : ControllerBase
     {
-       
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]CourseViewModel courseViewModel)
+        {
+            courseService.Create(courseViewModel);
+
+            return Ok(courseViewModel);
+        }
     }
 }
